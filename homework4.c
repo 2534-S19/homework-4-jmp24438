6bf7188317332503b79c4e43b8ef4c003244cbab
jmp24438 <60836234+jmp24438@users.noreply.github.com>
2020-04-09 22:21:28 -0400

    I have not written the code for FSM due to shortage of time and some other lack of resources that I was facing couple of days ago.

--- homework4.c
+++ homework4.c
@@ -7,22 +7,35 @@ int main(void)
     char *response = "\n\n\r2534 is the best course in the curriculum!\r\n\n";
 
     // TODO: Declare the variables that main uses to interact with your state machine.
-
+    bool finished = false;
 
     // Stops the Watchdog timer.
     initBoard();
     // TODO: Declare a UART config struct as defined in uart.h.
     //       To begin, configure the UART for 9600 baud, 8-bit payload (LSB first), no parity, 1 stop bit.
-
+    eUSCI_UART_ConfigV1 uartConfig =
+    {
+         EUSCI_A_UART_CLOCKSOURCE_SMCLK,               // SMCLK Clock Source = 3MHz
+         19,                                           // UCBR   = 19
+         8,                                            // UCBRF  = 8
+         0x55,                                         // UCBRS  = 0x55
+         EUSCI_A_UART_NO_PARITY,                       // No Parity
+         EUSCI_A_UART_LSB_FIRST,                       // LSB First
+         EUSCI_A_UART_ONE_STOP_BIT,                    // One stop bit
+         EUSCI_A_UART_MODE,                            // UART mode
+         EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION // Oversampling
+         EUSCI_A_UART_8_BIT_LEN                        // 8-bit payload
+    };
 
     // TODO: Make sure Tx AND Rx pins of EUSCI_A0 work for UART and not as regular GPIO pins.
-
+    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,GPIO_PIN2, GPIO_PRIMARY_MODULE_FUNCTION);
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P1,GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
 
     // TODO: Initialize EUSCI_A0
-
+    UART_initModule(EUSCI_A0_BASE, &uartConfig);
 
     // TODO: Enable EUSCI_A0
-
+    UART_enableModule(EUSCI_A0_BASE);
 
     while(1)
     {
